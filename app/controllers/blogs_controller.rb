9c136f63a1365404f4e36d77eb64c9469ed1dba9
motohiro takagi <tkg.8.mthr@gmail.com>
2021-05-27 22:14:24 +0900

    controllerにcreateメソッドを作成した

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,10 @@
 class BlogsController < ApplicationController
-    def index
-    end
+  def index
+  end
+  def new
+    @blog = Blog.new
+  end
+  def create
+    Blog.create(content: params[:content])
+  end
 end
--- db/migrate/20210527113432_posts.rb
+++ db/migrate/20210527113432_posts.rb
@@ -2,5 +2,6 @@ class Posts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text :content
+      t.timestamps
   end
 end
