be21cd3d23cd5730bc54cc19ad2678bea3909cd5
motohiro takagi <tkg.8.mthr@gmail.com>
2021-05-28 13:59:49 +0900

    Modelにエラーメッセージ表示条件コード追記

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,23 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
   def new
     @blog = Blog.new
   end
   def create
-    Blog.create(blog_params)
-    redirect_to new_blog_path
+    @blog = Blog.create(blog_params)
+    if @blog.save
+      redirect_to blog_path, notice: "ツイートを投稿しました"
+    else
+      render :new
+    end
   end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:content)
