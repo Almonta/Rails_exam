0d19edd34b5b1782d201c4433f6e9587b6e306cd
motohiro takagi <tkg.8.mthr@gmail.com>
2021-05-29 18:05:02 +0900

    header,footerのcssを追加した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,61 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
+    # content1 = @posts[0].content
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "ツイートを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+  
+  def show
+    #@post = Post.find(params[:id])
+  end
+  
+  def edit
+    #@post = Post.find(params[:id])
+  end
+  
+  def update
+    #@post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "ツイートを編集しました！"
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"ツイートを削除しました"
+  end
+  
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+  
+  
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
+
